cb786ad2e681deb03dbd57932bd06264158cb221
cyuyang@umich.edu <cyuyang@umich.edu>
2014-10-07 18:11:55 -0400

    Correct index.html and submit.js

--- js/submit.js
+++ js/submit.js
@@ -3,25 +3,25 @@ Parse.initialize("i3dGLONpG57AMgPVqquOKQQPJaFy64sYs8ObWxwQ", "AfaEsQlF2BE0Fk3ikO
 var Participant = Parse.Object.extend("Participant");
 var participant = new Participant();
 
-document.getElementById("sign-up").onclick = function() {
-    var fName = document.getElementById("lth-reg-Fname").value;
-    var lName = document.getElementById("lth-reg-Lname").value;
-    var uniqueName = document.getElementById("lth-reg-Uname").value;
-    var gender = document.getElementById("lth-reg-gender").value;
-    var grade = document.getElementById("lth-reg-grade").value;
+document.getElementById("lth-reg-button").onclick = function() {
+    var fName = document.getElementById("lth-reg-firstName").value;
+    var lName = document.getElementById("lth-reg-lastName").value;
+    var uniqName = document.getElementById("lth-reg-uniqname").value;
+    var _class = document.getElementById("lth-reg-class").value;
+    var experience = document.getElementById("lth-reg-experience").value;
     var major = document.getElementById("lth-reg-major").value;
     
     participant.set("FirstName", fName);
     participant.set("LastName", lName);
-    participant.set("UniqueName", uniqueName);
-    participant.set("Gender", gender);
-    participant.set("Grade", grade);
+    participant.set("UniqueName", uniqName);
+    participant.set("Class", _class);
     participant.set("Major", major);
+    participant.set("Experience", experience);
     
     participant.save(null, {
   success: function(participant) {
     // Execute any logic that should take place after the object is saved.
-    alert('New object created with objectId: ' + participant.id);
+    alert('Thanks for your registration');
   },
   error: function(participant, error) {
     // Execute any logic that should take place if the save fails.
